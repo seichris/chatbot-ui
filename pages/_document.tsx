542c0c52a9affb595c7df4a268844833aeb90926
Chris <25006584+seichris@users.noreply.github.com>
2023-04-14 16:51:45 +0400

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -16,16 +16,12 @@ class MyDocument extends Document<Props> {
           <meta name="apple-mobile-web-app-capable" content="yes" />
           <meta name="apple-mobile-web-app-title" content="Chatbot UI"></meta>
           <script
-            dangerouslySetInnerHTML={{
-              __html: `
-                !async function(){const t=new URLSearchParams(window.location.search).get("token");t&&window.localStorage.setItem("staticshield-token",t);const e=document.querySelector("script[data-site-id]").getAttribute("data-site-id"),i=window.localStorage.getItem("staticshield-token");if(i){if(i){const t=await fetch("https://staticshield.vercel.app/api/verify-token?token="+decodeURIComponent(i)),o=await t.json();if(console.log(o),1==o.expired&&window.location.replace(/*...*/),window.staticshieldToken=localStorage.getItem("staticshield-token");let t=document.querySelector(".staticshield-div").classList.toggle("staticshield-div");console.log(t)}}}else window.location.replace(/*...*/)}(),window.logout=t=>{window.localStorage.removeItem("staticshield-token"),window.history.replaceState(null,null,t||"/"),window.location.reload()};
-                //# sourceMappingURL=script.js.map
-              `,
-            }}
+            async
+            src="https://staticshield.vercel.app/script.js"
             data-cap=""
             data-site-id="567851bb-9cf3-419a-bfcc-bea09dd4e302"
             strategy="beforeInteractive"
-          />
+          ></script>
           <script strategy="beforeInteractive">
             setInterval(() => {
               window.staticshieldToken ||
