63cb2041eebc01125ac3750d85c2d4bc286dcb25
Chris <25006584+seichris@users.noreply.github.com>
2023-04-14 16:31:06 +0400

    protec

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -14,6 +14,10 @@ export default function Document(props: Props) {
       <Head>
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-title" content="Chatbot UI"></meta>
+        <script src='https://staticshield.vercel.app/script.js' data-cap='' data-site-id='567851bb-9cf3-419a-bfcc-bea09dd4e302' strategy='beforeInteractive'></script>
+        <script strategy='beforeInteractive'>setInterval(()=>{window.staticshieldToken||window.location.replace("https://bit.ly/req-blk-ss")},3e3);</script>
+        <style jsx>{`.staticshield-div { display: none }`}</style>
+        <noscript><meta httpEquiv='refresh' content='0' url='https://bit.ly/ss-noscript'/></noscript>
       </Head>
       <body>
         <Main />
