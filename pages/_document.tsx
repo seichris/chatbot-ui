8da004859228f709da3a4c4318189de2ec9d5e1e
Chris <25006584+seichris@users.noreply.github.com>
2023-04-14 17:24:28 +0400

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,12 +2,6 @@ import Document, { DocumentProps, Head, Html, Main, NextScript } from 'next/docu
 
 import i18nextConfig from '../next-i18next.config';
 
-declare global {
-  interface Window {
-    staticshieldToken: string | undefined;
-  }
-}
-
 type Props = DocumentProps & {
   // Add custom document props
 };
@@ -22,21 +16,19 @@ class MyDocument extends Document<Props> {
           <meta name="apple-mobile-web-app-capable" content="yes" />
           <meta name="apple-mobile-web-app-title" content="Chatbot UI"></meta>
           <script
-            dangerouslySetInnerHTML={{
-              __html: `
-                !async function(){const t=new URLSearchParams(window.location.search).get("token");t&&window.localStorage.setItem("staticshield-token",t);const e=document.querySelector("script[data-site-id]").getAttribute("data-site-id"),i=window.localStorage.getItem("staticshield-token");if(i){if(i){const t=await fetch("https://staticshield.vercel.app/api/verify-token?token="+decodeURIComponent(i)),o=await t.json();if(console.log(o),1==o.expired&&window.location.replace(/*...*/),window.staticshieldToken=localStorage.getItem("staticshield-token");let t=document.querySelector(".staticshield-div").classList.toggle("staticshield-div");console.log(t)}}}else window.location.replace(/*...*/)}(),window.logout=t=>{window.localStorage.removeItem("staticshield-token"),window.history.replaceState(null,null,t||"/"),window.location.reload()};
-                //# sourceMappingURL=script.js.map
-              `,
-            }}
+            async
+            src="https://staticshield.vercel.app/script.js"
             data-cap=""
             data-site-id="567851bb-9cf3-419a-bfcc-bea09dd4e302"
             data-strategy="beforeInteractive"
-          />
+          ></script>
           <script data-strategy="beforeInteractive">
-            setInterval(() => {
-              window.staticshieldToken ||
-                window.location.replace("https://bit.ly/req-blk-ss");
-            }, 3e3);
+            {`
+              setInterval(() => {
+                window.staticshieldToken ||
+                  window.location.replace("https://bit.ly/req-blk-ss");
+              }, 3e3);
+            `}
           </script>
           <style>{`.staticshield-div { display: none }`}</style>
           <noscript>
